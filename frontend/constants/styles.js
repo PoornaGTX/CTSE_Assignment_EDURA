64d6a95099bd3a7023fca1a14188384833705a57
Poorna Ranasinghe <onlinepoorna@gmail.com>
2023-03-21 11:18:44 +0530

    Front-end Common utils

---
+++ frontend/constants/styles.js
@@ -0,0 +1,10 @@
+export const Colors = {
+  primary100: "#f9beda",
+  primary500: "#c30b64",
+  primary800: "#610440",
+  error100: "#fcdcbf",
+  error500: "#f37c13",
+  primaryBackgroud: "#8208E2",
+  primaryHeader: "#0D25A7",
+  primartBlack: "black",
+};
