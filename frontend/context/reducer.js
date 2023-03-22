edcf37f70cd988bd64b83e9d06901129d7c276a1
ThameeraFernando <sithnuwan111@gmail.com>
2023-03-22 21:00:51 +0530

    add student screens and update appContext

--- frontend/context/reducer.js
+++ frontend/context/reducer.js
@@ -6,6 +6,15 @@ import {
   LOGIN_USER_SUCCESS,
   LOGIN_USER_ERROR,
   LOGOUT_BEGIN,
+  STUDENT_GET_ALL_NOTICES_BEGIN,
+  STUDENT_GET_ALL_NOTICES_SUCCESS,
+  STUDENT_GET_ALL_NOTICES_ERROR,
+  GET_USERS_BEGIN,
+  GET_USERS_SUCCESS,
+  GET_USERS_ERROR,
+  SUBSCRIBE_TEACHER_BEGIN,
+  SUBSCRIBE_TEACHER_SUCCESS,
+  SUBSCRIBE_TEACHER_END,
 } from "./action";
 
 const reducer = (state, action) => {
@@ -77,6 +86,88 @@ const reducer = (state, action) => {
       showAlert: true,
       alertType: "success",
       alertText: "Login Successful! Redirecting",
+    };
+  }
+
+  //get users
+  if (action.type === GET_USERS_BEGIN) {
+    return { ...state, isLoading: true, showAlert: false };
+  }
+
+  if (action.type === GET_USERS_SUCCESS) {
+    return {
+      ...state,
+      isLoading: false,
+      showAlert: true,
+      users: action.payload.users,
+      alertType: "success",
+      alertText: "User Created! Redirecting",
+    };
+  }
+
+  if (action.type === GET_USERS_ERROR) {
+    return {
+      ...state,
+      isLoading: false,
+      showAlert: true,
+      alertType: "danger",
+      alertText: "hello",
+    };
+  }
+
+  //subscribe handler
+  if (action.type === SUBSCRIBE_TEACHER_BEGIN) {
+    return {
+      ...state,
+      isLoading: true,
+    };
+  }
+
+  if (action.type === SUBSCRIBE_TEACHER_SUCCESS) {
+    return {
+      ...state,
+      isLoading: false,
+      showAlert: true,
+      alertType: "success",
+      alertText: "Success",
+    };
+  }
+
+  if (action.type === SUBSCRIBE_TEACHER_END) {
+    return {
+      ...state,
+      isLoading: false,
+      showAlert: true,
+      alertType: "danger",
+      alertText: action.payload.msg,
+    };
+  }
+
+  if (action.type === STUDENT_GET_ALL_NOTICES_BEGIN) {
+    return { ...state, isLoading: true, showAlert: false };
+  }
+
+  if (action.type === STUDENT_GET_ALL_NOTICES_SUCCESS) {
+    return {
+      ...state,
+      isLoading: false,
+      showAlert: true,
+      studentNotices: action.payload.notices,
+      alertType: "success",
+      alertText: "Success",
+      // mySubscribeList: action.payload.users.find(
+      //   (user) => user._id === action.payload.userId
+      // ).subscribeIds,
+    };
+  }
+
+  if (action.type === STUDENT_GET_ALL_NOTICES_ERROR) {
+    return {
+      ...state,
+      isLoading: false,
+      showAlert: true,
+      alertType: "danger",
+      alertText: "hello",
     };
   }
 
